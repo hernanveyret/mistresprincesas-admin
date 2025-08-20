30d346225a01b6b460caa56c491fa1b77771c227
hernanveyret <hernanveyret@hotmail.com>
2025-08-19 22:52:03 -0300

    fix url and qr

--- src/Componentes/Compartir.jsx
+++ src/Componentes/Compartir.jsx
@@ -5,7 +5,7 @@ const Compartir = ({setSharedLink, setOnQr}) => {
 
     // copia al portapapeles la url
    const sharedApp = () => {
-    navigator.clipboard.writeText('https://mistresprinsesas.vercel.app/') 
+    navigator.clipboard.writeText('https://mistresprincesas.vercel.app/') 
     .then(() => {
       setSharedLink(true)
       setTimeout(() => {
